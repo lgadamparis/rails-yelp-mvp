f4809b2c679eeb60de882453acfe8312a3c02c37
louis gonzague adam <lg.adam@mac.com>
2021-05-12 22:50:58 +0200

    YELP MVP exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require 'faker'
+
+5.times do
+  Restaurant.create({ name: Faker::Restaurant.name, address: Faker::Address.street_address, category: 'french',
+                      phone_number: Faker::PhoneNumber.cell_phone })
+end
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
